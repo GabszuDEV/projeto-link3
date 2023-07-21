1f226ab20182ed2f9a11b896939b053dcda25325
Sunsy <gabszu.dev@gmail.com>
2023-07-21 11:02:09 -0300

    :tada: Avatar bug fix

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, add a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/Avatar-light.png")
   } else {
     // se tiver sem light mode, manter a img normal
     img.setAttribute("src", "./assets/avatar.png")
